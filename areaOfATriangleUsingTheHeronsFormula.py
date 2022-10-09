ae8c78e1c80283355200499af0066f704ee36660
MSI\antar <alaktalapatra@gmail.com>
2022-10-10 00:17:24 +0530

    area of a triangle using the herons formula

---
+++ areaOfATriangleUsingTheHeronsFormula.py
@@ -0,0 +1,6 @@
+a=float(input("Enter the length for the first ide of the triangle(A) : "))
+b=float(input("Enter the length for the second side of the triangle(B) :"))
+c=float(input("Enter the length for the third side of the triangle(C) :"))
+s= (a+b+c)/2 #s is the semi perimeter
+area = (s*((s-a)*(s-b)*(s-c)))**0.5
+print("The area of the triangle using the Heron's formula is :",area)
