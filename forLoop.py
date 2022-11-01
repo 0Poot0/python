40a6c3b8a4999ca90223e161d425d30a9ed226b8
MSI\antar <alaktalapatra@gmail.com>
2022-11-01 22:22:48 +0530

    01-11-22

---
+++ forLoop.py
@@ -0,0 +1,11 @@
+#cars=["Nano","Ford","Tata","BMW","Thar"]
+#for i in cars:
+ #   if i=="Tata":
+  #      break
+   # print(i)
+
+cars=["Nano","Ford","Tata","BMW","Thar"]
+for i in cars:
+    if i=="Tata":
+        continue
+    print(i)
