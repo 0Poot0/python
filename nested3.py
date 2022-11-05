1189ebf0c80167c8fcbd3e1dc94d5b4e32452a03
MSI\antar <alaktalapatra@gmail.com>
2022-11-05 21:47:03 +0530

    05-11-22

---
+++ nested3.py
@@ -0,0 +1,12 @@
+numbers=[12,75,150,180,145]
+for i in numbers:
+    if i%5==0:
+        if i<=150:
+            if i>500:
+                break
+            print(i)
+i=0
+for i in numbers:
+    
+
+            
