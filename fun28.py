a4fd68f0c7a7cbcebe6358c6b1cd97cfc9f15963
MSI\antar <alaktalapatra@gmail.com>
2022-11-21 23:16:36 +0530

    21-11-22

---
+++ fun28.py
@@ -0,0 +1,20 @@
+numbers=[1,2,3,4,5,2,6]
+#for i in numbers:
+ #   if i==2:
+ #       a=numbers.index(i)
+ #       numbers.pop(numbers.index(2))
+ #       numbers.insert(a,200)
+ #       break
+#print(numbers)
+
+#indx=numbers.index(2)
+#numbers[indx]=200
+#print(numbers)
+
+#num2=numbers.copy()
+#print(num2)
+
+num2=list(numbers)
+print(num2)
+
+
