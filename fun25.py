82fb8544d4eebffef9e2c33c974730b6aeb81179
MSI\antar <alaktalapatra@gmail.com>
2022-11-24 22:05:37 +0530

    24-11-22

---
+++ fun25.py
@@ -0,0 +1,30 @@
+colors=["Red","Blue","Black",22,"Blue"]
+cars=["TATA","NANO","ALTO"]
+players=("Dhoni","Messi","Dravid")
+newList=[]
+#for i in cars:
+    if "A" in i:
+        newList.append(i)
+#print(newList)
+
+#new=[x for x in cars if x =="TATA"]
+#print(new)
+        
+#[print(x) for x in cars]
+#for x in range(len(cars)):
+    #print(cars[x])
+#while x<len(cars):
+
+    #print(cars[x])
+    #x+=13
+#colors[0:2]=["Yellow","Green"]
+#colors.insert(2,"Indigo")
+#colors.append("BMW")
+#print(colors)
+#colors.extend(players)
+#colors.pop(1)
+#print(colors)
+#del colors[0]
+#print(colors)
+#colors.clear()
+#print(colors)
