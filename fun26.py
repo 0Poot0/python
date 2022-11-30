d5f2e8a599c1b3e2eff9b99b386dbcc0363d2e1d
MSI\antar <alaktalapatra@gmail.com>
2022-11-30 22:08:49 +0530

    30-11-22

---
+++ fun26.py
@@ -0,0 +1,12 @@
+colors=["Red","Blue","Black",22,"Blue"]
+cars=["TATA","NANO","ALTO"]
+numbers=[1,2,5,4,3,7]
+
+cars.sort()
+numbers.sort()
+print(numbers)
+
+numbers.reverse()
+print(numbers)
+#newList=[x.lower() for x in cars]
+#print(newList)
