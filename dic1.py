a05e9025cb07f33424c27a547a676b8b567d4b85
MSI\antar <alaktalapatra@gmail.com>
2022-12-27 23:03:02 +0530

    27-12-22

---
+++ dic1.py
@@ -0,0 +1,14 @@
+myDictionary={
+    "class":{
+        "student":{
+            "name":"abc",
+            "marks":{
+                "python":99,
+                "web":30
+            }
+        }
+    }
+}
+#print(myDictionary)
+
+print(myDictionary["class"]["student"]["marks"]["web"])
