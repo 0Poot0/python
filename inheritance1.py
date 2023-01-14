434ca7335689edfdf820ea615422ac011d9cb060
MSI\antar <alaktalapatra@gmail.com>
2023-01-14 20:45:37 +0530

    14-01-23

---
+++ inheritance1.py
@@ -0,0 +1,12 @@
+#class A:
+ #   def method1(self):
+  #      print("This is method 1")
+
+#class B(A):
+ #   def method2(self):
+  #      print("This is method 2")
+
+#objA=A()
+#objB=B()
+
+#objB.method1()
