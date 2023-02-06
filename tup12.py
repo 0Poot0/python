2680ad4eb319b43e8bee11e368659e7c6fbfe3f4
MSI\antar <alaktalapatra@gmail.com>
2023-02-06 22:22:34 +0530

    06-02-23

---
+++ tup12.py
@@ -0,0 +1,12 @@
+tuple1=(10,20,30,40)
+#(num1,num2,num3,num4)=tuple1
+#print(num1,num2,num3,num4)
+
+#for i in tuple1:
+ #   print(i)
+
+i=0
+a=len(tuple1)
+while(i<a):
+    print(tuple1[i])
+    i=i+1
