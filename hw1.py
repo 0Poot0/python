f1a42383bd421ab88ee5682b1a9120310815ae32
MSI\antar <alaktalapatra@gmail.com>
2023-02-25 21:20:43 +0530

    25-02-23

--- hw1.py
+++ hw1.py
@@ -1,10 +1,9 @@
-lower = 900
-upper = 1000
+lower = int(input())
+upper = int(input())
 
 print("Prime numbers between", lower, "and", upper, "are:")
 
 for num in range(lower, upper + 1):
-   # all prime numbers are greater than 1
    if num > 1:
        for i in range(2, num):
            if (num % i) == 0:
